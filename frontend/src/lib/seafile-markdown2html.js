105c39d3ea88c396a3f979c9b019bdc765d3688a
ilearnit <helloworld.c@outlook.com>
2018-05-02 14:09:58 +0800

    seafile-editor

---
+++ frontend/src/lib/seafile-markdown2html.js
@@ -0,0 +1,34 @@
+var unified = require('unified');
+var remark = require('remark');
+var markdown = require('remark-parse');
+var remark2rehype = require('remark-rehype');
+var format = require('rehype-format');
+var raw = require('rehype-raw');
+var xtend = require('xtend');
+var toHTML = require('hast-util-to-html');
+var sanitize = require('hast-util-sanitize');
+// var github = require('hast-util-sanitize/lib/github');
+
+
+function stringify(config) {
+  var settings = xtend(config, this.data('settings'));
+
+  this.Compiler = compiler;
+
+  function compiler(tree) {
+    // use sanity to remove dangerous html, the default is
+    // GitHub style sanitation
+    var hast = sanitize(tree);
+    return toHTML(hast, settings);
+  }
+}
+
+// markdown -> mdast -> html AST -> html
+var processor = unified()
+  .use(markdown, {commonmark: true})
+  .use(remark2rehype, {allowDangerousHTML: true})
+  .use(raw)
+  .use(format)
+  .use(stringify);
+
+  export default processor;
